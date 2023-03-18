362e3dac18c069f4139dd0944bd2ce6d81ec1eb6
zoria93 <artemzora4@gmail.com>
2023-03-18 17:50:45 +0200

    Update App.jsx

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -15,6 +15,19 @@ export class App extends Component {
     ],
     filter: '',
   };
+  componentDidMount() {
+    const contacts = localStorage.getItem('contacts');
+    const parsedContacts = JSON.parse(contacts);
+    if (parsedContacts) {
+      this.setState({ contacts: parsedContacts });
+    }
+  }
+
+  componentDidUpdate(preprops, prevState) {
+    if (this.state.contacts !== prevState.contacts) {
+      localStorage.setItem('contacts', JSON.stringify(this.state.contacts));
+    }
+  }
 
   addContact = (name, number) => {
     const duplicateName = this.state.contacts.find(
